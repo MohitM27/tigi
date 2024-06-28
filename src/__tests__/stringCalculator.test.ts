e44f5cfb020cec96cb6c63f450cfb2ec2b34b9b2
Mohit <mohit@vividwebtechnologies.com>
2024-06-29 01:22:56 +0530

    Modify: add validation in string

--- src/__tests__/stringCalculator.test.ts
+++ src/__tests__/stringCalculator.test.ts
@@ -10,3 +10,9 @@ test('should return 1 for the input "1"', () => {
 test('should return 3 for the input "1,2"', () => {
     expect(add("1,2")).toBe(3);
 });
+test('should return 15 for the input "1,2,3,4,5"', () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+});
+test('should return 6 for the input "1\\n2,3"', () => {
+    expect(add("1\n2,3")).toBe(6);
+});
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,6 +2,7 @@ export function add(numbers: string): number {
     if (numbers === "") {
         return 0;
     }
+    numbers = numbers.replace(/\n/g, ",");
     const numArray = numbers.split(",").map(num => parseInt(num, 10));
     return numArray.reduce((sum, num) => sum + num, 0);
 }
